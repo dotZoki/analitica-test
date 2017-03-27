8e9aa5b2c2b2a39e003fb5d581643bd7511f5a2e
dotZoki <zoran.zoki@gmail.com>
2017-03-27 11:48:12 +0200

    balancing requests

---
+++ LoadBalancer/BalanceResource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using Grapevine.Client;
+using Grapevine.Interfaces.Server;
+using Grapevine.Server.Attributes;
+using Grapevine.Shared;
+
+namespace LoadBalancer
+{
+	public class BalanceResource
+	{
+		public BalanceResource()
+		{
+		}
+
+		[RestResource]
+		public class MonitorResource
+		{
+			[RestRoute]
+			public IHttpContext Answer(IHttpContext context)
+			{
+				//var client = new RESTClient("localhost:8900");
+				var client = new RESTClient("www.mocky.io");
+				Console.WriteLine("=========================");
+				Console.WriteLine(context.Request.PathInfo);
+				Console.WriteLine("=========================");
+				RESTResponse response = client.Execute(new RESTRequest("/v2/58d8dd6f0f0000721fdcc7af"));
+				Console.WriteLine(response.Content);
+
+				//context.Response.SendResponse(Encoding.UTF8.GetBytes("thump thump"));
+				context.Response.SendResponse(Encoding.UTF8.GetBytes(response.Content));
+
+				return context;
+			}
+		}
+
+	}
+}
---
+++ LoadBalancer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoadBalancer
+{
+	class MainClass
+	{
+		
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Load balancer for REST started on port 8888");
+
+			using (var server = new Grapevine.Server.RestServer())
+			{
+				server.Port = "8888";
+				server.LogToConsole().Start();
+				Console.ReadLine();
+				server.Stop();
+			}
+
+		}
+	}
+}
